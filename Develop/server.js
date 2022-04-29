9c537472afeb14ad506e13d809fd809920a49b6e
walterunderwood <walonsounderwood@gmail.com>
2022-04-28 20:41:36 -0700

    Creating get routes on server.js

--- Develop/server.js
+++ Develop/server.js
@@ -9,3 +9,18 @@ const PORT = process.env.PORT || 3001
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/notes", function(req, res){
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get("/api/notes", function(req, res){
+    fs.readFile('./db/db.json', 'utf-8', function(err, data) {
+        if (err) throw err
+        res.json(JSON.parse(data))
+    })
+});
