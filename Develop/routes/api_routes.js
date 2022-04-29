f93a9cd4eeebfe7eb92a09b582d06a92d310c1cb
walterunderwood <walonsounderwood@gmail.com>
2022-04-28 20:37:14 -0700

    Restructuring Files

--- Develop/routes/api_routes.js
+++ Develop/routes/api_routes.js
@@ -1,44 +1,44 @@
-const api = require('express').Router();
+// const api = require('express').Router();
 
-const {v4: uuid4 } = require('uuid');
+// const {v4: uuid4 } = require('uuid');
 
-const dataBase = require("../db/db.json");
+// const dataBase = require("../db/db.json");
 
-const fs = require('fs');
-const util = require(util);
-const readFromFile = util.promisify(fs.readFile);
-const writeFromFile = util.promisify(fs.writeFile);
+// const fs = require('fs');
+// const util = require(util);
+// const readFromFile = util.promisify(fs.readFile);
+// const writeFromFile = util.promisify(fs.writeFile);
 
 
-//Retrieving all the Notes
-api.get('/api/notes', function(req, res){
-    readFromFile(dataBase).then((data)=> res.json(JSON.parse(data)))
-});
+// //Retrieving all the Notes
+// api.get('/api/notes', function(req, res){
+//     readFromFile(dataBase).then((data)=> res.json(JSON.parse(data)))
+// });
 
-// Submitting all the Notes
-api.post('/api/notes', function(req, res){
+// // Submitting all the Notes
+// api.post('/api/notes', function(req, res){
 
    
-    const newNote = {
-        title,
-        text,
-        id: uuid4(),
-    };
-        fs.readFile(dataBase, 'UTF-8', (err, data) =>{
-        if (err){
-           console.log(err);
-        } else{
-            JSON.parse(data).push(newNote);
+//     const newNote = {
+//         title,
+//         text,
+//         id: uuid4(),
+//     };
+//     //     fs.readFile(dataBase, 'UTF-8', (err, data) =>{
+//     //     if (err){
+//     //        console.log(err);
+//     //     } else{
+//     //         JSON.parse(data).push(newNote);
             
-        }
-    })
+//     //     }
+//     // })
     
     
-    res.json('Note added succesfully!');
-    res.json('Error in posting Note');
+//     res.json('Note added succesfully!');
+//     res.json('Error in posting Note');
    
 
 
-});
+// });
 
-api.delete('')
+// api.delete('')
